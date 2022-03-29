165e7d2e996a2b0a68442ae0dc9531a56db19faf
mohit10r <mohitranjan28@gmail.com>
2022-03-29 11:21:57 +0530

    decimal_to_binary

---
+++ Recursion/decimal_to_binary.py
@@ -0,0 +1,9 @@
+def decimal_to_binary(n):
+    assert int(n)==n, 'The number must be an integer'
+
+    if n == 0:
+        return 0
+    else:
+        return n%2 + 10 * decimal_to_binary(int(n/2)) 
+
+print(decimal_to_binary(10))           
